746632ad26d875b9c85bccd3b0a76a71b5e5f957
ToOfan <toofaneariyayi@gmail.com>
2017-05-05 03:47:48 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -2,7 +2,7 @@ package.path = package.path..';.luarocks/share/lua/5.2/?.lua;.luarocks/share/lua
 package.cpath = package.cpath..';.luarocks/lib/lua/5.2/?.so'
     bot_token = "Token" --Put You Token Here
    send_api = "https://api.telegram.org/bot"..bot_token
-sudo_id = 157059515
+sudo_id = 111334847
 require('./bot/methods')
 http = require('socket.http')
 https = require('ssl.https')
